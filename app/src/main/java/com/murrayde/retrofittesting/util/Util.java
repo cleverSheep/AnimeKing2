d8a96bbf3e362a178f47c223236e6eb1bbcbd1e6
cleverSheep <clarmoph1029@gmail.com>
2020-01-02 18:37:19 -0500

    finished changing the design of each item in the recyclerview. next, work on creating the detail fragment to navigate to.

--- app/src/main/java/com/murrayde/retrofittesting/util/Util.java
+++ app/src/main/java/com/murrayde/retrofittesting/util/Util.java
@@ -7,6 +7,10 @@ import androidx.databinding.BindingAdapter;
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.CenterInside;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.murrayde.retrofittesting.R;
 
@@ -15,6 +19,7 @@ public class Util {
     private static void loadImage(ImageView imageView, String url, CircularProgressDrawable circularProgressDrawable) {
         RequestOptions options = new RequestOptions()
                 .placeholder(circularProgressDrawable)
+                .transform(new CenterCrop(), new RoundedCorners(16))
                 .error(R.mipmap.ic_launcher_round);
         Glide.with(imageView.getContext())
                 .setDefaultRequestOptions(options)
