8067d1d7d7d67fb9492d6ed4a45b098270019a8e
Dean Murray <murraydean1029@gmail.com>
2020-03-10 15:44:15 -0400

    complete: create a list header layout. start: modify adapter to use the header viewholder

--- app/src/main/java/com/murrayde/animeking/view/community/list_anime/AnimeList.kt
+++ app/src/main/java/com/murrayde/animeking/view/community/list_anime/AnimeList.kt
@@ -15,14 +15,12 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.murrayde.animeking.R
 import com.murrayde.animeking.network.community.api.AnimeData
 import com.murrayde.animeking.util.PagingUtil
-import com.murrayde.animeking.view.community.viewmodel.AnimeListMotionStateViewModel
 import com.murrayde.animeking.view.community.viewmodel.MainActivityViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 class AnimeList : Fragment() {
 
     private lateinit var listAdapter: AnimeListAdapter
-    private lateinit var motionState_viewModel: AnimeListMotionStateViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -37,7 +35,6 @@ class AnimeList : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         listAdapter = AnimeListAdapter()
         val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        motionState_viewModel = ViewModelProvider(this).get(AnimeListMotionStateViewModel::class.java)
 
         rv_anime.adapter = listAdapter
         rv_anime.layoutManager = GridLayoutManager(activity!!, 3)
