9e2beb3a7810779e979fee1a71682f2ddabec243
Dean <murrayde17@students.ecu.edu>
2020-03-11 12:21:00 -0400

    complete: merge master with develop

--- app/src/main/java/com/murrayde/animeking/view/MainActivity.kt
+++ app/src/main/java/com/murrayde/animeking/view/MainActivity.kt
@@ -7,64 +7,24 @@ import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 
 import androidx.appcompat.app.AppCompatActivity
-<<<<<<< HEAD
-=======
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
->>>>>>> develop
 
 import com.murrayde.animeking.R
 import com.murrayde.animeking.util.PagingUtil
 import com.murrayde.animeking.util.QuestionUtil
-<<<<<<< HEAD
-import com.murrayde.animeking.view.community.list_anime.AnimeList
-import com.murrayde.animeking.view.profile.ProfileFragment
-import com.murrayde.animeking.view.search.SearchFragment
-import com.murrayde.animeking.view.settings.SettingsFragment
-import com.pandora.bottomnavigator.BottomNavigator
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navigator: BottomNavigator
-
-=======
-
-class MainActivity : AppCompatActivity() {
->>>>>>> develop
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-<<<<<<< HEAD
-
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(View(this), InputMethodManager.SHOW_IMPLICIT)
-
-        navigator = BottomNavigator.onCreate(
-                fragmentContainer = R.id.nav_host_fragment,
-                bottomNavigationView = findViewById(R.id.bottomnav_view),
-                rootFragmentsFactory = mapOf(
-                        R.id.menu_nav_home to { AnimeList() },
-                        R.id.menu_nav_search to { SearchFragment() },
-                        R.id.menu_nav_profile to { ProfileFragment() },
-                        R.id.menu_nav_settings to { SettingsFragment() }
-                ),
-                defaultTab = R.id.menu_nav_home,
-                activity = this
-        )
-    }
-
-    override fun onBackPressed() {
-        if (!navigator.pop()) {
-            super.onBackPressed()
-        }
-=======
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.showSoftInput(View(this), InputMethodManager.SHOW_IMPLICIT)
 
         val navController = findNavController(R.id.nav_host_fragment)
         findViewById<BottomNavigationView>(R.id.bottom).setupWithNavController(navController)
->>>>>>> develop
     }
 }
--- app/src/main/java/com/murrayde/animeking/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/murrayde/animeking/view/settings/SettingsFragment.kt
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-package com.murrayde.animeking.view.settings
-
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
-import com.murrayde.animeking.R
-
-/**
- * A simple [Fragment] subclass.
- */
-class SettingsFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
-
-
-}
-=======
 package com.murrayde.animeking.view.settings
 
 
@@ -45,4 +19,3 @@ class SettingsFragment : Fragment() {
 
 
 }
->>>>>>> develop
