1724a91549cd5694685bbf4a7d7f4ec8208e8023
Dean Murray <clarmoph1029@gmail.com>
2020-07-16 09:11:44 -0400

    ANDP 4: Retrieve one or more questions fro firestore

--- app/src/main/java/com/murrayde/animekingmobile/network/community/clients/FirebaseApiClient.kt
+++ app/src/main/java/com/murrayde/animekingmobile/network/community/clients/FirebaseApiClient.kt
@@ -1,9 +1,9 @@
 package com.murrayde.animekingmobile.network.community.clients
 
 import androidx.core.os.LocaleListCompat
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
 import com.murrayde.animekingmobile.model.community.CommunityQuestion
-import com.murrayde.animekingmobile.util.QuestionUtil
 import com.murrayde.animekingmobile.util.removeForwardSlashes
 import de.aaronoe.rxfirestore.getSingle
 import io.reactivex.Single
@@ -15,7 +15,20 @@ class FirebaseApiClient @Inject constructor() {
     /** Questions for quiz session*/
     fun getQuestions(anime_title: String): Single<List<CommunityQuestion>> {
         val questionsRef = firebaseDB.collection("anime").document(LocaleListCompat.getDefault()[0].language).collection("titles")
-                .document(removeForwardSlashes(anime_title)).collection("questions").limit(QuestionUtil.QUESTION_LIMIT)
-        return questionsRef.getSingle()
+                .document(removeForwardSlashes(anime_title)).collection("questions")
+
+        val key = questionsRef.document().id
+
+/*
+        val listOfQuestions = mutableListOf<CommunityQuestion>()
+        var questionSize = 0L
+        questionsRef.whereGreaterThanOrEqualTo(FieldPath.documentId(), key).limit(questionSize).get().addOnSuccessListener { documents ->
+            documents.forEach {
+                listOfQuestions.add(it.toObject(CommunityQuestion::class.java))
+            }
+        }
+        return Single.just(listOfQuestions)
+*/
+        return questionsRef.whereGreaterThanOrEqualTo(FieldPath.documentId(), key).getSingle()
     }
 }
--- app/src/main/java/com/murrayde/animekingmobile/util/QuestionUtil.java
+++ app/src/main/java/com/murrayde/animekingmobile/util/QuestionUtil.java
@@ -1,7 +1,7 @@
 package com.murrayde.animekingmobile.util;
 
 public class QuestionUtil {
-    public static final Long QUESTION_LIMIT = 10L;
+    public static final Long QUESTION_LIMIT = 7L;
     public static final Long QUESTION_COUNT_MIN = 7L;
     public static Long QUESTION_TIMER = 11000L;
 }
