92de1d5bf671a094f597b3ab6e77b6d97abd0a53
Dean Murray <murraydean1029@gmail.com>
2021-02-01 21:37:37 -0500

    ANDP-37-Update-Add-More-Questions-Screen

--- app/src/main/java/com/murrayde/animekingmobile/screen/main/detail/LowQuestionCount.kt
+++ app/src/main/java/com/murrayde/animekingmobile/screen/main/detail/LowQuestionCount.kt
@@ -5,15 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.navigation.ui.setupWithNavController
 import com.murrayde.animekingmobile.R
-import com.murrayde.animekingmobile.screen.main.detail.AnimeListDetailArgs
-import com.murrayde.animekingmobile.screen.main.detail.LowQuestionCountDirections
 import kotlinx.android.synthetic.main.fragment_low_question_count.*
 
 class LowQuestionCount : Fragment() {
@@ -22,10 +17,7 @@ class LowQuestionCount : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_low_question_count, container, false)
-        val toolbar = view.findViewById<Toolbar>(R.id.toolbar_low_questions)
-        toolbar.setupWithNavController(findNavController())
-        return view
+        return inflater.inflate(R.layout.fragment_low_question_count, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
